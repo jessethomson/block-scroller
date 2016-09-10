99c53984334b35370a5fd066524412f09410d266
Jesse Thomson <jessethomson11@gmail.com>
2016-09-10 13:56:21 -0600

    Added game loop with pausing/starting/stopping and player movement

--- js/app.js
+++ js/app.js
@@ -1,33 +1,88 @@
-$("#player").load("./templates/characters/mario-standing.html")
-//$("#player2").load("./templates/characters/luigi-standing.html")
-var player = document.getElementById("player");
+(function() {
+	var character = "mario";
+	var player = $("#player");
+	var paused;
+	var game;
+	var gameWidth = $(window).width();
+	var gameHeight = $(window).height();
 
-$(document.body).on('keydown', function(e) {
-	switch (e.which) {
-		case 37:
-			console.log('left arrow key pressed!');
-			//player.css("left","100px");
-			break;
-		case 39:
-			console.log('right arrow key pressed!');
-			break;
-		case 32:
-			playAudio("jump-sound");
-			jump("mario");
-			break;
-		case 16:
-			console.log("shift pressed");
-			break;
+	var enemies = [];
+
+	function startGame() {
+		
+		stopGame();
+		player.load("./templates/characters/" + character + ".html");
+		paused = false;
+
+		window.setTimeout(function(){
+			game = window.setInterval(function() {
+				if(!paused) {
+					// update
+					shift(player, 5)
+				}
+			}, 30)
+		}, 1000); // one second pause
 	}
-});
 
-function playAudio(className) {
-	document.getElementById(className).play();
-}
+	function stopGame() {
+		player.css("left", gameWidth/8);
+		player.css("top", gameHeight/2);
+		clearInterval(game);
+	}
 
-function jump(characterName) {
-	$("#player").load("./templates/characters/" + characterName + "-jumping.html");
-	window.setTimeout(function() {
-		$("#player").load("./templates/characters/" + characterName + "-standing.html")
-	}, 350);
-}
+	$(document.body).on('keydown', function(e) {
+		switch (e.which) {
+			case 80: // 'p' pressed
+				paused = true;
+				break;
+			case 81: // 'q' pressed
+				stopGame();
+				break;
+			case 83: // 's' pressed
+				startGame();
+				break;
+			case 37: // 'left arrow' pressed 
+				console.log('left arrow key pressed!');
+				break;
+			case 39: // 'right arrow' pressed
+				console.log('right arrow key pressed!');
+				break;
+			case 32: // 'space bar' pressed
+				if(paused) {
+					paused = false;
+				}
+				else {
+					jump();
+				}
+				break;
+			default: // temporary
+				console.log(e.which);
+		}
+	});
+
+	function playAudio(className) {
+		document.getElementById(className).play();
+	}
+
+	function jump() {
+		if(!paused) {
+			playAudio("jump-sound");
+			player.load("./templates/characters/" + character + "-jumping.html")
+			window.setTimeout(function() {
+				player.load("./templates/characters/" + character + ".html")
+			}, 350);
+		}
+	}
+
+	function loadPlayer(character) {
+		player.load("./templates/characters/" + character + ".html");
+		var width = player.children();
+		console.log(width);
+	}
+
+	function shift(element, amount) {
+		element.css("left", parseFloat(element.css("left")) + amount);
+	}
+
+	startGame();
+}());
